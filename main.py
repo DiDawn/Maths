80a70a19c870747f82f6196c4ce0b19b32c84ab0
DiDawn <95550246+DiDawn@users.noreply.github.com>
2022-07-09 05:43:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,59 @@
-import math
-import random
-import time
+import pygame
+import ctypes
 
+ctypes.windll.user32.SetProcessDPIAware()
 
-def table():
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
+pygame.init()
 
-    start_time = time.time()
-    result = int(input(f'{a} * {b} = '))
-    end_time = time.time()
+# initialisation of pygame module
+pygame.init()
 
-    if result == a * b:
-        print(f'temps = {end_time - start_time}')
+# summon the game's window / .set_caption is used to change the name
+pygame.display.set_caption("Math Gym")
+
+# height and width of the window
+info_display_size = pygame.display.Info()
+screen = pygame.display.set_mode((info_display_size.current_w, info_display_size.current_h))
+
+# loading the background
+background = pygame.image.load('Assets/menu_bg.jpg')
+
+# loading the start buttons
+play_button_off = pygame.image.load('Assets/small-start-button-off2.png')
+play_button_on = pygame.image.load('Assets/small-start-button-on2.png')
+# play_button = pygame.transform.scale(play_button, (400, 150))
+play_button_rect = play_button_on.get_rect()
+play_button_rect.x = int(info_display_size.current_w/2 - play_button_rect.w/2)
+play_button_rect.y = int(info_display_size.current_h/5*3 + play_button_rect.h/1.5)
+
+running = True
+
+clock = pygame.time.Clock()
+# loop of the game
+while running:
+    # set the fps
+    clock.tick(144)
+
+    # apply the background
+    screen.blit(background, (0, 0))
+
+    # grab the cursor coordinate
+    cursor_pos = pygame.mouse.get_pos()
+    # print(f'cursor_pos:{cursor_pos}  collide:{play_button_rect.collidepoint(cursor_pos)}')
+    # display the appropriate play button
+    if play_button_rect.collidepoint(cursor_pos):
+
+        screen.blit(play_button_on, play_button_rect)
     else:
-        print(f'Nuuuuul : {a} * {b} = {a*b}')
+        screen.blit(play_button_off, play_button_rect)
 
+    # update the frame :
+    pygame.display.flip()
 
-def perfect_square(multiplied=False):
-    a = random.randint(1, 10)
-    a_square = a*a
-    multiplier = None
+    # get the events
+    for event in pygame.event.get():
 
-    if multiplied:
-        multiplier = random.randint(1, 10)
-
-    start_time = time.time()
-
-    if multiplied:
-        result = input(f'√{a_square * multiplier} = ')
-
-    else:
-        result = input(f'√{a_square} = ')
-
-    end_time = time.time()
-
-    if (int(result.replace('r', '')) == a and not multiplied) or \
-            (result == f'{a}r{multiplier}' and multiplied) or\
-            int(result.split('r')[0]) * math.sqrt(int(result.split('r')[1])) or\
-            int(result.replace('r', '')) == math.sqrt(a_square * multiplier):
-        print(f'temps = {end_time - start_time}')
-
-    else:
-        if multiplied:
-            print(f'nuuul ! result = {math.sqrt(a_square * multiplier)}\n ou result = {a}√{multiplier}')
-        else:
-            print(f'nuuul! result = {a}')
-
-
-perfect_square(multiplied=True)
+        # detect if the window need to be closed
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
